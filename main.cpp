bbd647e992dce67e7beb2e9b44b25f6239511c38
Joao Pedro <jpsa101106@gmail.com>
2026-03-03 14:56:25 -0300

    adicionando nomes dos integrantes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+//Joao Pedro Santana Alves 2267
+//Adriano Alves Silva 2269
+//Bernardo Moreira Moura Botta 754
+//Lucas Alberto Silva e Barros 846
+//Enzo Dias Venancio Guimaraes Nunes 2328
+
 #include <iostream>
 #include "cabecalho.h"
 using namespace std;
